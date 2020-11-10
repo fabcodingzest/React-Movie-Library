bc5faee935a8916473ff02f8f2352be1005155ed
fabcodingzest <fabrizvi786@gmail.com>
2020-11-10 22:08:02 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import tmdbApi from "./api/api";
+const getData = async () => {
+  const result = await tmdbApi.get("movie/550", {
+    params: {
+      append_to_response: "videos",
+    },
+  });
+  console.log(result.data);
+};
 
 function App() {
+  getData();
+
   return (
-    <div className=" flex items-start">
+    <div className="flex items-start">
       <Sidebar />
     </div>
   );
