edc532c442ddd865e74fb058496f246da051f09e
fabcodingzest <fabrizvi786@gmail.com>
2020-11-18 17:23:22 +0530

    Refactored SideBar code

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -3,8 +3,31 @@ import StickyBox from "react-sticky-box";
 import MovieIllustration from "../assets/movietime.jpg";
 import { Link } from "react-router-dom";
 
+const renderStatic = (categories) => {
+  return categories.map((category, index) => (
+    <Link
+      key={index}
+      to={`${process.env.PUBLIC_URL}/discover/${category}`}
+      className="block"
+    >
+      {category}
+    </Link>
+  ));
+};
+
+const renderGenres = (genres) => {
+  return genres.map((genre) => (
+    <Link
+      className="block"
+      key={genre.id}
+      to={`${process.env.PUBLIC_URL}/genre/${genre.name}`}
+    >
+      {genre.name}
+    </Link>
+  ));
+};
+
 const Sidebar = ({ genres, config }) => {
-  console.log("ko");
   const staticCategories = ["Popular", "Top Rated", "Upcoming"];
   return (
     <StickyBox className="border sidebar border-gray-300 p-4 min-h-screen">
@@ -15,26 +38,10 @@ const Sidebar = ({ genres, config }) => {
           alt="Movie illustration"
         />
         <h1 className="font-bold text-md">Discover</h1>
-        {staticCategories.map((category, index) => (
-          <Link
-            key={index}
-            to={`${process.env.PUBLIC_URL}/discover/${category}`}
-            className="block"
-          >
-            {category}
-          </Link>
-        ))}
+        {renderStatic(staticCategories)}
         <br />
         <h1 className="font-bold text-md">Genres</h1>
-        {genres.map((genre) => (
-          <Link
-            className="block"
-            key={genre.id}
-            to={`${process.env.PUBLIC_URL}/genre/${genre.name}`}
-          >
-            {genre.name}
-          </Link>
-        ))}
+        {renderGenres(genres)}
       </div>
     </StickyBox>
   );
