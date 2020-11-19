e27442c14a05725e87e86957bcd3399451856341
fabcodingzest <fabrizvi786@gmail.com>
2020-11-19 22:05:37 +0530

    Added constant folder for constants

---
+++ src/constants/state.js
@@ -0,0 +1,12 @@
+export const INITIAL_APP_STATE = {
+  loadingApp: true,
+  genres: [],
+  config: {},
+  errors: [],
+};
+
+export const INITIAL_MOVIES_STATE = {
+  loadingMovies: true,
+  movies: {},
+  errors: [],
+};
