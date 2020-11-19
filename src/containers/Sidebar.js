3cdbf18c07ddf9626be288835680f4e14b33172c
fabcodingzest <fabrizvi786@gmail.com>
2020-11-19 22:19:28 +0530

    Styled sidebar

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,16 +1,48 @@
 import React from "react";
 import StickyBox from "react-sticky-box";
-import MovieIllustration from "../assets/movietime.jpg";
+import MovieIllustration from "../assets/movietime.svg";
+import TMDBLogo from "../assets/TMDBLogo.svg";
 import { Link } from "react-router-dom";
+import ListItem from "../components/ListItem";
+import { faGift, faFire, faChartLine } from "@fortawesome/free-solid-svg-icons";
 
-const renderStatic = (categories) => {
+const Sidebar = ({ genres, config }) => {
+  const staticCategories = ["Popular", "Top Rated", "Upcoming"];
+  const staticIcons = [faGift, faFire, faChartLine];
+  console.log("sidebar");
+  return (
+    <StickyBox className="border-r-2 sidebar border-gray-300 text-gray-500 min-h-screen">
+      <img
+        className="w-full pt-6 px-6"
+        src={MovieIllustration}
+        alt="Movie illustration"
+      />
+      <div className="sidebar p-4">
+        <h1 className="text-gray-700 font-bold text-lg">Discover</h1>
+        {renderStatic(staticCategories, staticIcons)}
+        <br />
+        <h1 className="text-gray-700 font-bold text-lg">Genres</h1>
+        {renderGenres(genres)}
+      </div>
+      <p className="flex justify-center items-center mt-4 text-md">
+        &copy; Copyright
+        <a href="https://github.com/fabcodingzest" className="font-bold ml-2">
+          Fab
+        </a>
+      </p>
+      <img
+        className="w-full px-16 py-6"
+        src={TMDBLogo}
+        alt="The Movie DB Logo"
+      />
+    </StickyBox>
+  );
+};
+
+const renderStatic = (categories, staticIcons) => {
   return categories.map((category, index) => (
-    <Link
-      key={index}
-      to={`${process.env.PUBLIC_URL}/discover/${category}`}
-      className="block"
-    >
-      {category}
+    <Link key={index} to={`${process.env.PUBLIC_URL}/discover/${category}`}>
+      <ListItem text={category} icon={staticIcons[index]} />
     </Link>
   ));
 };
@@ -22,29 +54,9 @@ const renderGenres = (genres) => {
       key={genre.id}
       to={`${process.env.PUBLIC_URL}/genre/${genre.name}`}
     >
-      {genre.name}
+      <ListItem text={genre.name} />
     </Link>
   ));
-};
-
-const Sidebar = ({ genres, config }) => {
-  const staticCategories = ["Popular", "Top Rated", "Upcoming"];
-  return (
-    <StickyBox className="border sidebar border-gray-300 p-4 min-h-screen">
-      <div className="sidebar">
-        <img
-          className="mb-10 w-16"
-          src={MovieIllustration}
-          alt="Movie illustration"
-        />
-        <h1 className="font-bold text-md">Discover</h1>
-        {renderStatic(staticCategories)}
-        <br />
-        <h1 className="font-bold text-md">Genres</h1>
-        {renderGenres(genres)}
-      </div>
-    </StickyBox>
-  );
 };
 
 export default Sidebar;
