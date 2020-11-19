60d87cee126f8307d90f0101c1af27c015d5fb12
fabcodingzest <fabrizvi786@gmail.com>
2020-11-19 22:05:02 +0530

    Updated listitem component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
-function ListItem(props) {
+function ListItem({ text, icon }) {
   return (
-    <div className="pl-4 mt-4 text-sm font-semibold">
-      <FontAwesomeIcon icon={props.icon ? props.icon : faFilm} />
-      <span className="p-2">{props.text}</span>
+    <div className="pl-4 mt-4 text-sm">
+      <FontAwesomeIcon icon={icon ? icon : faFilm} />
+      <span className="p-2">{text}</span>
     </div>
   );
 }
