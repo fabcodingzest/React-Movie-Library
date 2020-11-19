e6483a24609f4e9a07b0f407a57a732c4b1b4699
fabcodingzest <fabrizvi786@gmail.com>
2020-11-20 00:16:54 +0530

    changed selected style in navbar

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,8 +5,10 @@ function ListItem({ text, icon, selected }) {
   return (
     <div
       className={`pl-4 py-2 mt-2 text-sm rounded-full border border-transparent ${
-        selected && "bg-gray-700 text-gray-100"
-      } hover:border-gray-500`}
+        selected
+          ? "bg-gray-700 text-gray-100 border-transparent"
+          : "hover:border-gray-500"
+      }`}
     >
       <FontAwesomeIcon icon={icon ? icon : faFilm} />
       <span className="p-2">{text}</span>
