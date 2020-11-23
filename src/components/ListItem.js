94febdde1d30b7b387fca2f6fcf5248d6510d6c3
fabcodingzest <fabrizvi786@gmail.com>
2020-11-23 22:46:00 +0530

    changing list item styles as we moved it to sidebar component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
-function ListItem({ text, icon, selected }) {
+function ListItem({ text, icon }) {
   return (
-    <div
-      className={`pl-4 py-2 mt-2 text-sm rounded-full border border-transparent ${
-        selected
-          ? "bg-gray-700 text-gray-100 border-transparent"
-          : "hover:border-gray-500"
-      }`}
-    >
+    <div className="text-lg sm:text-sm inline-block">
       <FontAwesomeIcon icon={icon ? icon : faFilm} />
       <span className="p-2">{text}</span>
     </div>
