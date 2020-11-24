22b770bc46fa35bb3ac386d6b2c4e470bd22c502
fabcodingzest <fabrizvi786@gmail.com>
2020-11-24 23:22:25 +0530

    Scroll and Pagination added

--- src/containers/Discover.js
+++ src/containers/Discover.js
@@ -5,7 +5,9 @@ import Loader from "../components/Loader";
 import { getDiscoverMovies } from "../helpers/MoviesHelpers";
 import MoviesReducer from "../reducers/MoviesReducer";
 import { INITIAL_MOVIES_STATE } from "../constants/state";
+import { animateScroll as scroll } from "react-scroll";
 import queryString from "query-string";
+import Pagination from "../components/Pagination";
 
 function Discover({ location, match, baseURL, setSelected }) {
   const category = match.params.name;
@@ -14,6 +16,10 @@ function Discover({ location, match, baseURL, setSelected }) {
   const [state, dispatch] = useReducer(MoviesReducer, INITIAL_MOVIES_STATE);
 
   useEffect(() => {
+    scroll.scrollToTop({
+      smooth: true,
+      delay: 400,
+    });
     setSelected(category);
     getDiscoverMovies(
       dispatch,
@@ -41,11 +47,14 @@ function Discover({ location, match, baseURL, setSelected }) {
   }
 
   return (
-    <div className="text-gray-600 pt-24 min-h-screen flex flex-col justify-center">
+    <div className="text-gray-600 pt-32 min-h-screen flex flex-col justify-center">
       <h1 className="text-3xl w-full font-thin uppercase ml-4">{category}</h1>
       <p className="text-sm uppercase font-bold ml-4">movies</p>
       <div className="text-gray-700 my-8">
         <MovieList movies={movies} baseURL={baseURL} />
+      </div>
+      <div className="w-full mb-12">
+        <Pagination movies={movies} />
       </div>
     </div>
   );
