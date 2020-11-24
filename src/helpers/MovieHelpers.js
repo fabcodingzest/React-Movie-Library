7ef1ac5c03fcfc8b9a0115a60dde0f3f053a6634
fabcodingzest <fabrizvi786@gmail.com>
2020-11-24 23:18:17 +0530

    Changes in Movie Reducer and helper for individual state in movie component

--- src/helpers/MovieHelpers.js
+++ src/helpers/MovieHelpers.js
@@ -3,21 +3,32 @@ import tmdb from "../api/api";
 async function getMovieDetails(dispatch, movieId) {
   dispatch({ type: "movie_loading" });
   try {
-    const movie = await tmdb.get(`movie/${movieId}`);
+    const movie = await tmdb.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
     await dispatch({ type: "fetch_movie", payload: movie.data });
     const castDetails = await tmdb.get(`movie/${movieId}/credits`);
     await dispatch({ type: "fetch_cast", payload: castDetails.data.cast });
-    const recommendedMovies = await tmdb.get(
-      `movie/${movieId}/recommendations`
-    );
-    await dispatch({
-      type: "fetch_movie_recommendations",
-      payload: recommendedMovies.data,
-    });
     dispatch({ type: "movie_loaded" });
   } catch (error) {
     dispatch({ type: "movie_fetch_failed", payload: error });
   }
 }
-
-export { getMovieDetails };
+async function getRecommendations(dispatch, movieId, page) {
+  try {
+    dispatch({ type: "movie_recommendation_loading" });
+    const recommendedMovies = await tmdb.get(
+      `movie/${movieId}/recommendations`,
+      {
+        params: { page },
+      }
+    );
+    dispatch({
+      type: "fetch_movie_recommendations",
+      payload: recommendedMovies.data,
+    });
+  } catch (error) {
+    dispatch({ type: "movie_fetch_failed", payload: error });
+  }
+}
+export { getMovieDetails, getRecommendations };
--- src/reducers/MovieReducer.js
+++ src/reducers/MovieReducer.js
@@ -2,17 +2,23 @@ function MovieReducer(state, action) {
   switch (action.type) {
     case "movie_loading":
       return { ...state, loadingMovie: true };
+    case "movie_recommendation_loading":
+      return { ...state, loadingRecommendations: true };
     case "fetch_movie":
       return { ...state, movieDetails: action.payload };
     case "fetch_cast":
       return { ...state, castDetails: action.payload };
     case "fetch_movie_recommendations":
-      return { ...state, recommendedMovies: action.payload };
+      return {
+        ...state,
+        recommendedMovies: action.payload,
+        loadingRecommendations: false,
+      };
     case "movie_loaded":
       return { ...state, loadingMovie: false };
     case "movie_fetch_failed":
-      return { ...state, errors: [action.payload.message] };
     case "cast_fetch_failed":
+      return { ...state, errors: [action.payload.message] };
     default:
       return state;
   }
