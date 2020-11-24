215ee5746c497d1d637cb3d115101a1c640611fb
fabcodingzest <fabrizvi786@gmail.com>
2020-11-24 23:17:42 +0530

    changed Person Reducer and helper to manage the state individually

--- src/helpers/PersonHelper.js
+++ src/helpers/PersonHelper.js
@@ -1,14 +1,8 @@
 import tmdb from "../api/api";
 
-async function getPersonComponent(dispatch, personId, page, sort) {
-  dispatch({ type: "fetch_person_loaded" });
-  await getMovieDetails(dispatch, personId);
-  await getPersonMovies(dispatch, personId, page, sort);
-  dispatch({ type: "fetch_person_loading" });
-}
-
-async function getMovieDetails(dispatch, personId) {
+async function getPersonDetails(dispatch, personId) {
   try {
+    dispatch({ type: "fetch_person_loading" });
     const person = await tmdb.get(`person/${personId}`);
     dispatch({ type: "fetch_person", payload: person.data });
   } catch (error) {
@@ -18,7 +12,8 @@ async function getMovieDetails(dispatch, personId) {
 
 async function getPersonMovies(dispatch, personId, page, sort) {
   try {
-    const recommendedMovies = await tmdb.get(`/discover/movie`, {
+    dispatch({ type: "fetch_person_movies_loading" });
+    const personMovies = await tmdb.get(`/discover/movie`, {
       params: {
         with_cast: personId,
         page,
@@ -26,12 +21,12 @@ async function getPersonMovies(dispatch, personId, page, sort) {
       },
     });
     dispatch({
-      type: "fetch_person_recommendations",
-      payload: recommendedMovies.data,
+      type: "fetch_person_movies",
+      payload: personMovies.data,
     });
   } catch (error) {
     dispatch({ type: "person_fetch_failed", payload: error });
   }
 }
 
-export default getPersonComponent;
+export { getPersonDetails, getPersonMovies };
--- src/reducers/PersonReducer.js
+++ src/reducers/PersonReducer.js
@@ -1,17 +1,17 @@
 function PersonReducer(state, action) {
   switch (action.type) {
-    case "fetch_person_loaded":
+    case "fetch_person_loading":
       return { ...state, loadingPerson: true };
+    case "fetch_person_movies_loading":
+      return { ...state, loadingMovies: true };
     case "fetch_person":
-      return { ...state, personDetails: action.payload };
-    case "fetch_person_recommendations":
+      return { ...state, personDetails: action.payload, loadingPerson: false };
+    case "fetch_person_movies":
       return {
         ...state,
         personMovies: action.payload,
         loadingMovies: false,
       };
-    case "fetch_person_loading":
-      return { ...state, loadingPerson: false };
     case "person_fetch_failed":
       return { ...state, errors: [action.payload.message] };
     default:
