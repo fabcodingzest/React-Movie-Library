0e1e0c09c0d0a6caee22a368de6c7d6fdd1cf54f
fabcodingzest <fabrizvi786@gmail.com>
2020-11-26 04:45:15 +0530

    changed loader to use for both app load and image load

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Loader() {
+function Loader({ imageLoader }) {
   return (
-    <div className="w-full flex justify-center items-center my-auto">
-      <div className="loader animate-spin"></div>
+    <div className={`${imageLoader ? "w-8 h-8" : "w-12 h-12"}`}>
+      <div className="h-full w-full loader animate-spin rounded-full"></div>
     </div>
   );
 }
